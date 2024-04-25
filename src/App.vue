9ff30a682d5f2c17d4ad47de0c21cebeb2b08822
chaud1024 <chaud1024@gmail.com>
2024-04-25 14:01:11 +0900

    about lifecyle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,14 @@
 <script setup>
-import { provide } from 'vue'
+import LifecycleComponent from './components/LifecycleComponent.vue'
 
-import ProvideComponent from './components/ProvideComponent.vue'
-
-provide('name', 'Paul')
-provide('age', 20)
-provide('location', ['Paris', 'France'])
-
-provide('friends', ['Johanson', 'Alex', 'Harry'])
-
-provide('games', {
-  id: 1,
-  title: 'Game of Thrones',
-  genre: ['Action', 'Fantasy', 'Drama'],
-  platform: ['PS4', 'Xbox', 'PC'],
-  relaseDate: '2023-03-15',
-  developer: {
-    name: 'George Martin',
-    location: 'San Francisco, USA'
-  }
-})
-
-provide('movies', [
-  {
-    title: 'Inception',
-    genre: ['Action', 'Sci-Fi', 'Drama'],
-    year: 2010,
-    casts: ['Leonardo Di Caprio', 'Cillian Murphy', 'Domhnall Gleeson']
-  },
-  {
-    title: 'Interstellar',
-    genre: ['Sci-Fi', 'Drama', 'Adventure'],
-    year: 2014,
-    casts: ['Matthew McConaughey', 'Anne Hathaway', 'Jessica Chastain']
-  },
-  {
-    title: 'The Prestige',
-    genre: ['Drama', 'Mystery', 'Thriller'],
-    year: 2006,
-    casts: ['Christian Bale', 'Hugh Jackman', 'Scarlett Johansson']
-  }
-])
+import { ref } from 'vue'
+const showHide = ref(true)
 </script>
 
 <template>
   <div>
-    <ProvideComponent />
+    <LifecycleComponent v-if="showHide" />
+    <button @click="showHide = !showHide">Show / Hide</button>
   </div>
 </template>
 
---
+++ src/components/LifecycleComponent.vue
@@ -0,0 +1,51 @@
+<script setup>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue'
+
+const message = ref('Hello, vue!')
+
+// Lifecycle hooks
+onBeforeMount(() => {
+  console.log('Component is about to be mounted')
+})
+
+onMounted(() => {
+  console.log('Component has been mounted')
+})
+
+const updateMessage = () => {
+  message.value = 'Updated message'
+}
+
+onBeforeUpdate(() => {
+  console.log('Component is about to be updated')
+})
+
+onUpdated(() => {
+  console.log('Component has been updated')
+})
+
+onBeforeUnmount(() => {
+  console.log('Component is about to be unmounted')
+})
+
+onUnmounted(() => {
+  console.log('Component has been unmounted')
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="updateMessage">Update message</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
