7fc279b94280e5a2e590ffb64f85c60a9a4f9aed
chaud1024 <chaud1024@gmail.com>
2024-04-25 17:06:38 +0900

    about Composable

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useCounter } from '../shared/useCounter'
+
+const counter = useCounter()
+</script>
+
+<template>
+  <div>
+    <h1>Counter</h1>
+    <h2>{{ counter.count }}</h2>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
